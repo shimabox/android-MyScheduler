0eaef6f7c924ee80e042da6bf97ca970c392f4ea
shimabox <shimabox@gmail.com>
2020-12-27 21:55:30 +0900

    :bug: Realmのモデルクラスは継承させるためopenをつける

--- app/src/main/java/net/shimabox/myscheduler/Schedule.kt
+++ app/src/main/java/net/shimabox/myscheduler/Schedule.kt
@@ -4,7 +4,7 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
-class Schedule : RealmObject() {
+open class Schedule : RealmObject() {
     @PrimaryKey
     var id: Long = 0
     var date: Date = Date()
