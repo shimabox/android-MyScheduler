15270a642c986d67f656445aeb0152314950bffb
shimabox <shimabox@gmail.com>
2020-12-28 00:09:05 +0900

    :sparkles: 日付選択用ダイアログ生成クラス

--- app/src/main/java/net/shimabox/myscheduler/Dialogs.kt
+++ app/src/main/java/net/shimabox/myscheduler/Dialogs.kt
@@ -1,9 +1,12 @@
 package net.shimabox.myscheduler
 
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
 class ConfirmDialog(
         private val message: String,
@@ -22,5 +25,22 @@ class ConfirmDialog(
             cancelSelected()
         }
         return builder.create()
+    }
+}
+
+class DateDialog(private val onSelected: (String) -> Unit)
+    : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(requireActivity(), this, year, month, date)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int,
+                           month: Int, dayOfMonth: Int) {
+        onSelected("$year/${month + 1}/$dayOfMonth")
     }
 }
