cc528be2fcb61bc181a25dd4b0ce9c9c3b10e1cd
shimabox <shimabox@gmail.com>
2020-12-28 00:03:31 +0900

    :sparkles: 確認用ダイアログ生成クラス作成

---
+++ app/src/main/java/net/shimabox/myscheduler/Dialogs.kt
@@ -0,0 +1,26 @@
+package net.shimabox.myscheduler
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class ConfirmDialog(
+        private val message: String,
+        private val okLabel: String,
+        private val okSelected: () -> Unit,
+        private val cancelLabel: String,
+        private val cancelSelected: () -> Unit)
+    : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setMessage(message)
+        builder.setPositiveButton(okLabel) { dialog, which ->
+            okSelected()
+        }
+        builder.setNegativeButton(cancelLabel) { dialog, which ->
+            cancelSelected()
+        }
+        return builder.create()
+    }
+}
