1a3ccd7957d29894bcfb16b9d5086e4e796a9228
shimabox <shimabox@gmail.com>
2020-12-27 12:47:23 +0900

    :sparkles: 画面遷移のコードを追加

--- app/src/main/java/net/shimabox/myscheduler/MainActivity.kt
+++ app/src/main/java/net/shimabox/myscheduler/MainActivity.kt
@@ -6,33 +6,29 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import net.shimabox.myscheduler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val naviController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(naviController)
+
+        binding.fab.setOnClickListener { view ->
+            naviController.navigate(R.id.action_to_scheduleEditFragment)
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    fun setFavVisible(visibility: Int) {
+        binding.fab.visibility = visibility
     }
 }
