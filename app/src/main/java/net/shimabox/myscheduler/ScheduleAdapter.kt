87ef2602d0e2dc124a5a567a5e8ae05e14c94df6
shimabox <shimabox@gmail.com>
2020-12-27 23:04:17 +0900

    :+1: RecyclerViewのセルがタップされたことをRecyclerViewのアダプターからアクティビティへ通知するコールバック

--- app/src/main/java/net/shimabox/myscheduler/ScheduleAdapter.kt
+++ app/src/main/java/net/shimabox/myscheduler/ScheduleAdapter.kt
@@ -12,6 +12,12 @@ import io.realm.RealmRecyclerViewAdapter
 class ScheduleAdapter(data: OrderedRealmCollection<Schedule>) :
     RealmRecyclerViewAdapter<Schedule, ScheduleAdapter.ViewHolder>(data, true) {
 
+    private var listener: ((Long?) -> Unit)? = null
+
+    fun setOnItemClickListener(listener:(Long?) -> Unit) {
+        this.listener = listener
+    }
+
     init {
         setHasStableIds(true)
     }
@@ -31,6 +37,9 @@ class ScheduleAdapter(data: OrderedRealmCollection<Schedule>) :
         val schedule: Schedule? = getItem(position)
         holder.date.text = DateFormat.format("yyyy/MM/dd HH:mm", schedule?.date)
         holder.title.text = schedule?.title
+        holder.itemView.setOnClickListener {
+            listener?.invoke(schedule?.id)
+        }
     }
 
     override fun getItemId(position: Int): Long {
