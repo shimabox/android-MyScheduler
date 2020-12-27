8a3175dc7590ff9915e07f321b063fadca88de23
shimabox <shimabox@gmail.com>
2020-12-27 23:38:17 +0900

    :sparkles: スケジュール更新処理

--- app/src/main/java/net/shimabox/myscheduler/ScheduleEditFragment.kt
+++ app/src/main/java/net/shimabox/myscheduler/ScheduleEditFragment.kt
@@ -2,11 +2,13 @@ package net.shimabox.myscheduler
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.snackbar.Snackbar
 import io.realm.Realm
 import io.realm.kotlin.createObject
@@ -37,28 +39,56 @@ class ScheduleEditFragment : Fragment() {
         return binding.root
     }
 
+    private val args: ScheduleEditFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if (args.scheduleId != -1L) {
+            val schedule = realm.where<Schedule>()
+                    .equalTo("id", args.scheduleId).findFirst()
+            binding.dateEdit.setText(DateFormat.format("yyyy/MM/dd", schedule?.date))
+            binding.timeEdit.setText(DateFormat.format("HH:mm", schedule?.date))
+            binding.titleEdit.setText(schedule?.title)
+            binding.detailEdit.setText(schedule?.detail)
+        }
         (activity as? MainActivity)?.setFavVisible(View.INVISIBLE)
         binding.save.setOnClickListener { saveSchedule(it) }
     }
 
     private fun saveSchedule(view: View) {
-        realm.executeTransaction { db: Realm ->
-            val maxId = db.where<Schedule>().max("id")
-            val nextId = (maxId?.toLong() ?: 0L) + 1L
-            val schedule = db.createObject<Schedule>(nextId)
-            val date = "${binding.dateEdit.text} ${binding.timeEdit.text}".toDate()
+        when (args.scheduleId) {
+            -1L -> {
+                realm.executeTransaction { db: Realm ->
+                    val maxId = db.where<Schedule>().max("id")
+                    val nextId = (maxId?.toLong() ?: 0L) + 1L
+                    val schedule = db.createObject<Schedule>(nextId)
+                    val date = "${binding.dateEdit.text} ${binding.timeEdit.text}".toDate()
 
-            if (date != null) schedule.date = date
-            schedule.title = binding.titleEdit.text.toString()
-            schedule.detail = binding.detailEdit.text.toString()
+                    if (date != null) schedule.date = date
+                    schedule.title = binding.titleEdit.text.toString()
+                    schedule.detail = binding.detailEdit.text.toString()
+                }
+                Snackbar.make(view, "追加しました", Snackbar.LENGTH_SHORT)
+                        .setAction("戻る") { findNavController().popBackStack() }
+                        .setActionTextColor(Color.YELLOW)
+                        .show()
+            }
+            else -> {
+                realm.executeTransaction { db: Realm ->
+                    val schedule = db.where<Schedule>()
+                            .equalTo("id", args.scheduleId).findFirst()
+                    val date = "${binding.dateEdit.text} ${binding.timeEdit.text}".toDate()
+
+                    if (date != null) schedule?.date = date
+                    schedule?.title = binding.titleEdit.text.toString()
+                    schedule?.detail = binding.detailEdit.text.toString()
+                }
+                Snackbar.make(view, "修正しました", Snackbar.LENGTH_SHORT)
+                        .setAction("戻る") { findNavController().popBackStack() }
+                        .setActionTextColor(Color.YELLOW)
+                        .show()
+            }
         }
-
-        Snackbar.make(view, "追加しました", Snackbar.LENGTH_SHORT)
-            .setAction("戻る") { findNavController().popBackStack() }
-            .setActionTextColor(Color.YELLOW)
-            .show()
     }
 
     override fun onDestroyView() {
